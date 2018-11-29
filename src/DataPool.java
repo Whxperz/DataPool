f6db32bf53fb9eac32e8e6f0999a39abd8499bbf
Jayasri <Jayasri.Sundararajan.2022@student.lhschools.org>
2018-11-29 14:17:02 -0500

    Started Manufacturing Calculator

---
+++ src/DataPool.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DataPool {
+    public static void main(String[] args) {
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+
+        int days;
+        double totalCost;
+        double totalSales;
+        int supplies;
+        float sales;
+        float price1;
+
+
+        System.out.println("Welcome to the Manufacturing Calculator");
+        System.out.println();
+        System.out.println("Please enter the number of supplies that are required for your manufacturing process");
+        supplies = keyboard.nextInt();
+        System.out.println("Please enter the number of days of sale");
+        days = keyboard.nextInt();
+
+        for (int days = )
+
+
+
+
+
+    }
+}
