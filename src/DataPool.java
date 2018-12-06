87e91064bd1ea241c9a9c11516d564b95a7d7731
Jayasri <Jayasri.Sundararajan.2022@student.lhschools.org>
2018-12-06 14:19:37 -0500

    Finished Loops

--- src/DataPool.java
+++ src/DataPool.java
@@ -8,25 +8,47 @@ public class DataPool {
 
 
         int days;
-        double totalCost;
-        double totalSales;
-        int supplies;
+        int numberOfDays = 1;
+        int numberOfSupplies;
+        int numberOfItems;
+        int amountOfSupplies = 1;
+        int costOfSupplies = 0;
+        int productCost;
+        int supplyItem;
+        int amountSupply;
+        double totalProfit;
         float sales;
         float price1;
 
 
         System.out.println("Welcome to the Manufacturing Calculator");
         System.out.println();
-        System.out.println("Please enter the number of supplies that are required for your manufacturing process");
-        supplies = keyboard.nextInt();
+        System.out.println("Please enter the number of supplies required for your manufacturing process");
+        numberOfSupplies = keyboard.nextInt();
         System.out.println("Please enter the number of days of sale");
         days = keyboard.nextInt();
-
-        for (int days = )
-
-
-
-
+        do {
+            System.out.println("What is the cost of Supply Item " + amountOfSupplies + "?");
+            supplyItem = keyboard.nextInt();
+            System.out.println("How many did you buy of Supply Item " + amountOfSupplies + "?");
+            amountSupply = keyboard.nextInt();
+            productCost = (supplyItem*amountSupply);
+            costOfSupplies = (productCost+costOfSupplies);
+            amountOfSupplies++;
+        } while (amountOfSupplies <= numberOfSupplies);
+            System.out.println("The calculated total cost of all your supplies is: $" + costOfSupplies);
+        do {
+            System.out.println("How much did you sell each made item for on Day " + numberOfDays);
+            price1 = keyboard.nextInt();
+            System.out.println("How many items did you sell on Day " + numberOfDays);
+            numberOfItems = keyboard.nextInt();
+            sales = (numberOfItems*price1);
+            numberOfDays++;
+        } while (numberOfDays<=days);
+        System.out.println("The calculated total sales is: $" + sales);
+        totalProfit = (sales-costOfSupplies);
+        System.out.println();
+        System.out.println("The calculated profit is: $" + totalProfit);
 
     }
 }
